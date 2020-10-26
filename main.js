17f760384ffb704a08ee4f649a88f2a51fceb4c4
agustinrossi96 <73263518+agustinrossi96@users.noreply.github.com>
2020-10-26 20:58:48 -0300

    Add files via upload
    Subida de los archivos a GitHub

---
+++ main.js
@@ -0,0 +1,14 @@
+/*COMIENZO MOVIMIENTO MENU HAMBURGUESA*/
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click",() => {
+    if(!menuOpen) {
+        menuBtn.classList.add("open");
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove("open");
+        menuOpen = false;
+    
+    }
+}); 
+/*FIN MOVIMIENTO MENU HAMBURGUESA*/
